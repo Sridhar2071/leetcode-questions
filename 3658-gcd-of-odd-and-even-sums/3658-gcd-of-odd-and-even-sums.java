580c0325c7dd320a45395d939db5ae21ed8a325d
SURYAVAMSHI SRIDHAR PATIL <sridharpatilsuryavamshi@gmail.com>
2026-07-15 10:56:22 +0530

    Time: 1 ms (83.11%), Space: 42.6 MB (57.16%) - LeetHub

---
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int gcdOfOddEvenSums(int n) {
+        // int sumOdd=0;
+        // int sumEven=0;
+        // int ans=0;
+        // for(int i=1;i<=n*2;i=i+1){
+        //  if(i%2==0) sumEven+=i;
+        //  else sumOdd+=i;
+        // }
+        // for(int i=1;i<=sumEven;i++){
+        //     if(sumEven%i==0 && sumOdd%i==0){
+        //         ans=Math.max(i,ans);
+        //     }
+        // }
+        // return ans;
+        return n;
+}
+}
